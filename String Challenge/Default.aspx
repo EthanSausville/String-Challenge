7f618b72e9cb90b9fc676453221e2883e6029b5e
EthanSausville <ethan.sausville@gmail.com>
2018-02-23 11:56:44 -0800

    Upload

---
+++ String Challenge/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="String_Challenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>1. Reverse your Name</h2>
+            Input your Name:
+            <asp:TextBox ID="nameBox" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="challengeButton1" runat="server" Text="Reverse Your Name" OnClick="challengeButton1_Click" />
+            <br />
+            <asp:Label ID="resultLabel1" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <h2>2. Reverse this sequence: &quot;Luke,Leia,Han,Chewbacca&quot;</h2><br />
+            <asp:Button ID="challengeButton2" runat="server" Text="Reorder Items" OnClick="challengeButton2_Click" />
+            <br />
+            <asp:Label ID="resultLabel2" runat="server"></asp:Label>
+            <br />
+            <br />
+            <h2>3. Pad this sequence with *&#39;s: &quot;Luke,Leia,Han,Chewbacca&quot;</h2>
+            Desired String Length:
+            <asp:TextBox ID="stringSizeBox" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="challengeButton3" runat="server" Text="Pad Items" OnClick="challengeButton3_Click" />
+            <br />
+            <asp:Label ID="resultLabel3" runat="server"></asp:Label>
+            <br />
+            <br />
+            <h2>4. Fix an Ugly String</h2>
+            <p>Covert this: &quot;NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.&quot; to this &quot;Now is the time for all good men to come to the aid of their country.&quot;</p>
+            <asp:Button ID="challengeButton4" runat="server" Text="Solve" OnClick="challengeButton4_Click" />
+            <br />
+            <asp:Label ID="resultLabel4" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ String Challenge/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace String_Challenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string names = "Luke,Leia,Han,Chewbacca";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void challengeButton1_Click(object sender, EventArgs e)
+        {
+            string name = nameBox.Text;
+            string nameReversed = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+                nameReversed += name[i];
+            resultLabel1.Text = nameReversed;
+            nameBox.Text = "";
+        }
+
+        protected void challengeButton2_Click(object sender, EventArgs e)
+        {
+            string[] namesArray = names.Split(',');
+            string namesReversed = "";
+            for (int i = namesArray.Length - 1; i >= 0; i--)
+                namesReversed += namesArray[i] + "<br />";
+            resultLabel2.Text = namesReversed;
+        }
+
+        protected void challengeButton3_Click(object sender, EventArgs e)
+        {
+            string[] namesArray = names.Split(',');
+
+            int desiredStringSize;
+            if (stringSizeBox.Text == "") desiredStringSize = 14;
+            else desiredStringSize = int.Parse(stringSizeBox.Text);
+
+            for (int i = 0; i < namesArray.Length; i++)
+            {
+                int currentStringSize = namesArray[i].Length;
+                int paddingLeft = (desiredStringSize - currentStringSize) / 2;
+                namesArray[i] = namesArray[i].PadRight(desiredStringSize - paddingLeft, '*');
+                namesArray[i] = namesArray[i].PadLeft(desiredStringSize, '*');
+            }
+
+            string result = "";
+            for (int i = 0; i < namesArray.Length; i++) result += namesArray[i] + "<br />";
+            resultLabel3.Text = result;
+            stringSizeBox.Text = "";
+        }
+
+        protected void challengeButton4_Click(object sender, EventArgs e)
+        {
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+            puzzle = puzzle.Replace("remove-me", "");
+            puzzle = puzzle.ToLower();
+            puzzle = puzzle.Replace("z", "t");
+            puzzle = puzzle.Replace("now ", "Now ");
+
+            resultLabel4.Text = puzzle;
+        }
+    }
+}
